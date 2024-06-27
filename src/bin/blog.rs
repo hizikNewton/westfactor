e493a4415555fda731a7e4d4861819a320240b11
bamidele-dell <bamideleisaac11@gmail.com>
2024-06-27 13:41:26 +0100

    add 12 factor config

--- src/bin/blog.rs
+++ src/bin/blog.rs
@@ -2,15 +2,20 @@ use std::{env, io, net::TcpListener, sync::Mutex};
 use actix_web::web;
 use dotenv::dotenv;
 use sqlx::PgPool;
-use westfactor::{startup::run, state::AppState};
+use westfactor::{startup::run, state::AppState,};
+use westfactor::telemetry::{get_subscriber, init_subscriber};
 
 
 #[actix_rt::main]
 pub async fn main() -> io::Result<()> {
     dotenv().ok();
+    let subscriber = get_subscriber("west_blog".into(),"info".into(),std::io::stdout);
+    init_subscriber(subscriber);
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
-    let host_address = env::var("HOST_ADDR").expect("host address");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let application = Application::build(configuration.clone()).await?;
+
+    let application_task = tokio::spawn(application.run_until_stopped());
 
     let db_pool = PgPool::connect(&database_url).await.unwrap();
 
---
+++ src/blog/blogApp.rs
@@ -0,0 +1,45 @@
+
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+
+impl Application {
+    pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
+        let connection_pool = get_connection_pool(&configuration.database);
+        let email_client = configuration.email_client.client();
+
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
+        let listener = TcpListener::bind(address)?;
+        let port = listener.local_addr().unwrap().port();
+        let server = run(
+            listener,
+            connection_pool,
+            email_client,
+            configuration.application.base_url,
+            configuration.application.hmac_secret,
+            configuration.redis_uri,
+        )
+        .await?;
+
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new().connect_lazy_with(configuration.with_db())
+}
+
+pub struct ApplicationBaseUrl(pub String);
+
