5e7896624cccb0bd1c82547cc381f0794a821ab3
Isaac Bamidele <isaac.bamidele@wayapaychat.com>
2024-06-29 21:04:51 +0100

    started categories

--- src/routers/routes.rs
+++ src/routers/routes.rs
@@ -5,11 +5,18 @@ pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
 }
 
-
-pub fn user_routes(cfg: &mut web::ServiceConfig){
+pub fn user_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/users")
-        .route("/", web::post().to(post_new_user))
-        .route("/", web::get().to(get_all_users))
+            .route("/", web::post().to(post_new_user))
+            .route("/", web::get().to(get_all_users)),
+    );
+}
+
+pub fn category_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/categories")
+            .route("/", web::post().to(add_category))
+            .route("/", web::get().to(get_all_categories)),
     );
 }
